6fba057d416b9102c19b57a7abdb7d2471303888
josdem <joseluis.delacruz@gmail.com>
2021-05-29 16:40:28 -0400

    #2 Adding Google Java Format plugin

--- src/main/java/com/jos/dem/springboot/thymeleaf/controller/OrderController.java
+++ src/main/java/com/jos/dem/springboot/thymeleaf/controller/OrderController.java
@@ -1,12 +1,12 @@
 package com.jos.dem.springboot.thymeleaf.controller;
 
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 import com.jos.dem.springboot.thymeleaf.model.UserDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Slf4j
 @Controller
