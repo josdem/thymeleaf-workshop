0145e272af0f803137c058dc44d7b808287db2ec
josdem <joseluis.delacruz@gmail.com>
2021-05-29 10:38:22 -0400

    Starting new Thymeleaf Workshop :)

---
+++ src/main/java/com/jos/dem/springboot/thymeleaf/controller/OrderController.java
@@ -0,0 +1,13 @@
+package com.jos.dem.springboot.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OrderController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
