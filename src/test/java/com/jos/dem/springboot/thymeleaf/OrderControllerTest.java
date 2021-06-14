78d156369a55583af156841fe9312a5bda42b945
josdem <joseluis.delacruz@gmail.com>
2021-06-14 19:39:22 -0400

    #9 Formatting code

--- src/test/java/com/jos/dem/springboot/thymeleaf/OrderControllerTest.java
+++ src/test/java/com/jos/dem/springboot/thymeleaf/OrderControllerTest.java
@@ -1,5 +1,7 @@
 package com.jos.dem.springboot.thymeleaf;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,19 +9,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 class OrderControllerTest {
 
-    private final WebTestClient webTestClient;
+  private final WebTestClient webTestClient;
 
-    @Test
-    @DisplayName("Call index")
-    void shouldCallIndexPage() {
-        webTestClient.get().uri("/").exchange().expectStatus().isOk().expectBody(String.class).consumeWith(result -> {
-            assertTrue(result.getResponseBody().contains("<title>Store Locator</title>"), "should contains expected title");
-        });
-    }
+  @Test
+  @DisplayName("Call index")
+  void shouldCallIndexPage() {
+    webTestClient
+        .get()
+        .uri("/")
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectBody(String.class)
+        .consumeWith(
+            result -> {
+              assertTrue(
+                  result.getResponseBody().contains("<title>Store Locator</title>"),
+                  "should contains expected title");
+            });
+  }
 }
