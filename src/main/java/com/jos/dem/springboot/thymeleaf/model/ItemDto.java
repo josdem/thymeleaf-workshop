d830a4b97dd6eba68169b4c24dd6a13569699822
josdem <joseluis.delacruz@gmail.com>
2021-09-02 22:28:46 -0500

    #10 Deleting unused setters

--- src/main/java/com/jos/dem/springboot/thymeleaf/model/ItemDto.java
+++ src/main/java/com/jos/dem/springboot/thymeleaf/model/ItemDto.java
@@ -2,12 +2,8 @@ package com.jos.dem.springboot.thymeleaf.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
 @Getter
-@Setter
-@ToString
 @AllArgsConstructor
 public class ItemDto implements DomainTransferObject {
   private String name;
--- src/main/java/com/jos/dem/springboot/thymeleaf/model/UserDto.java
+++ src/main/java/com/jos/dem/springboot/thymeleaf/model/UserDto.java
@@ -1,11 +1,9 @@
 package com.jos.dem.springboot.thymeleaf.model;
 
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@Setter
 @ToString
 public class UserDto implements DomainTransferObject {
   private String zipCode;
